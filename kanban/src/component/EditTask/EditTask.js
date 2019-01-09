ba73871f68526b4960ea92fba41d27c9802a674c
plV7350 <carrington10@ksu.edu>
2019-01-09 13:27:52 -0600

    adding in the edit task

--- kanban/src/component/EditTask/EditTask.js
+++ kanban/src/component/EditTask/EditTask.js
@@ -8,16 +8,48 @@ class EditTask extends React.Component{
         task: this.props.location.state.task
     }
 
+    handleTaskChange = event => {
+        const taske = event.target.value
+        this.setState({ task: taske})
+        console.log(this.state.task)
+
+    }
+    handleSubmit = event => {
+        event.preventDefault();
+
+        
+          var postName = this.state.name
+          console.log(postName)
+           var postTask = this.state.task
+        // end of task object to pass
+    
+        axios.post('/editTask', {
+            id: this.state.id,
+            task: postTask
+             }).then(res => {
+           console.log(res)
+           this.props.history.push('/')
+        })// end of post to the api 
+    }
+
     render(){
         console.log("hello")
           console.log(this.state.id)
                 return(
                     <div>
-                     <h1>Edit Task </h1>
-                     <bt></bt>
-               <p>    {this.state.task} </p> 
-               <p>     {this.state.id} </p>
-                    </div>
+                <h1>Edit Task  </h1>
+                <line></line>
+                <Form onSubmit={this.handleSubmit}>
+                    
+                <br></br> 
+                <TextArea  placeholder={this.state.task} task="postTask" onChange={this.handleTaskChange}>
+                
+                    </TextArea>
+                <br></br>
+                <br></br>
+                    <Button type="submit"> Submit Edit </Button>
+                </Form>
+                </div>
                 )// end of rteturn 
             }// end of render 
 }// end of editTask
